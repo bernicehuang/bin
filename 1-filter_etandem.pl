8cfc402e78394633e2a09246829eab1cbe76d1e6
Bernice Huang <bernice710@gmail.com>
2013-03-20 17:01:21 -0300

    remove size.file as command line input

--- 1-filter_etandem.pl
+++ 1-filter_etandem.pl
@@ -1,13 +1,12 @@
 use strict;
 use warnings;
 
-my $sizefile = shift;
 my $etandem = shift;
 my $outfile = shift;
 
 
 if(!defined($outfile)) {
-	die "USAGE: perl $0 1-sizefile 2-etandemfile 3-outfile\n";
+	die "USAGE: perl $0 1-etandemfile 2-outfile\n";
 }
 
 
@@ -27,7 +26,7 @@ foreach $etandemfile (@etandemarray) {
 	print "File = $etandemfile\n";
 	
   # run script to see if there were any repeats detected in the sequence
-  $command = "perl filter_etandem.pl $sizefile $etandemfile $outfile";
+  $command = "perl filter_etandem.pl $etandemfile $outfile";
     `$command`;
     
 }
